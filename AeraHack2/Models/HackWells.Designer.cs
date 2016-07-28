3ed4b1d3987851c264530c821325f179b5b0b0b7
ro213314 <rdorr@aeraenergy.com>
2016-07-28 14:31:53 -0700

    info

--- AeraHack2/Models/HackWells.Designer.cs
+++ AeraHack2/Models/HackWells.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\tm206721\Source\Repos\AeraHack\AeraHack2\Models\HackWells.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\source-bak\AeraHack2\AeraHack2\Models\HackWells.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- HackApp/Controllers/HomeController.cs
+++ HackApp/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace HackApp.Controllers
         private const string baseUri = "https://aerahackproxy.azurewebsites.net/AeraHack2/api/";
         public ActionResult Index()
         {
+
+            claims
             using (HttpClient httpClient = new HttpClient())
             {
                 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1uQ19WWmNBVGZNNXBPWWlKSE1iYTlnb0VLWSIsImtpZCI6Ik1uQ19WWmNBVGZNNXBPWWlKSE1iYTlnb0VLWSJ9.eyJhdWQiOiJodHRwczovL3NjYXNtZmxvcGhvdG1haWwub25taWNyb3NvZnQuY29tL0FlcmFIYWNrMiIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzQ1ZGZlNWRlLWFmY2UtNDMwNy1iMmMwLTVkMmQ2YTczMThjYy8iLCJpYXQiOjE0NjQzMDI5NjQsIm5iZiI6MTQ2NDMwMjk2NCwiZXhwIjoxNDY0MzA2ODY0LCJhcHBpZCI6IjAzZTdiMjc5LTQxNjAtNDA4ZS1hZGI0LWQyNjcyNjgzNTUyZCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzQ1ZGZlNWRlLWFmY2UtNDMwNy1iMmMwLTVkMmQ2YTczMThjYy8iLCJvaWQiOiJiM2MyMWVmNy1hMmM1LTQ1MzgtYjEwMC1hZmU4NzMyNGRhNTUiLCJzdWIiOiJiM2MyMWVmNy1hMmM1LTQ1MzgtYjEwMC1hZmU4NzMyNGRhNTUiLCJ0aWQiOiI0NWRmZTVkZS1hZmNlLTQzMDctYjJjMC01ZDJkNmE3MzE4Y2MiLCJ2ZXIiOiIxLjAifQ.LcYMyRrVrSEgm33o4fWwcdaye8eAY4AFq-ZCcOv42xBNKddQ7CAkp2WHabUhyJBP9JTN2kTBS7-BxSIMmn51xgcIEs9Mm_8RFe1ZT8E74BRhKb8gjqVC_XyM_SzS9PLGbzC_YmxoGzcTx1ReVSNxc9ETju-PBQNt4xMElcl5XwpDzIG52hpsZ1jlyqie2myCrrJJ2VkAvhHg6w1igcrO6miNNhINPzriSuo3tI7gdqVLfjDqA-KjPFCluMDxQlfLND7t6L4f1GknMwYVqLrUi_1Bvtg2iy4DwznpdkDpE1RPti1MdyWJk_aKrT69hphu13HRaoCx_WbQePb7Uygjfg";
